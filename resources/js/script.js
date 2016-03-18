f1f098fa1d18ca915e3166f00e5c7a9d03392ef2
frankiehuang <novofrankie@gmail.com>
2016-03-18 14:50:11 -0400

    Hello

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
+    
     /* Navigation Scroll */
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -32,5 +32,60 @@ $(document).ready(function() {
         }, 600);
         return false;
     });
+    
+        /*Sticky Nav*/
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down"){
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '160px;'
+    });
+    
+        /* Waypoint Animations on Scroll*/
+    
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+    },{
+        offset: '50%'
+    })
+    
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeInUp');
+    },{
+        offset: '50%'
+    })
+    
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn');
+    },{
+        offset: '50%'
+    })
+    
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated pulse');
+    },{
+        offset: '50%'
+    })
+    
+     /* Mobile Navigation*/
+    
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon')){
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon');
+        } else{
+            icon.removeClass('ion-close-round');
+            icon.addClass('ion-navicon');
+        }
+        
+    })
     
 });
